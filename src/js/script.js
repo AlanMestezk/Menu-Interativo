56597826c1c7efa12c11f387ec421644ccdaafa0
AlanMestezk <alanmestezk98@gmail.com>
2022-11-01 15:53:06 -0300

    Menu interativo com html, css e javaScript

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+
+
+const list = document.querySelectorAll('.list');
+
+function acitveLink()
+{
+
+    list.forEach
+    (
+        (item) => item.classList.remove('active')
+    )
+    
+    this.classList.add('active')
+
+}
+
+list.forEach
+(
+    (item) =>
+    item.addEventListener('click', acitveLink)
+)
+
